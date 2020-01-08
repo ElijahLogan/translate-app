25d9adcb56d3d973570b65dc3a40002fba9d92b9
Elijah Logan <elijah.logan@purchase.edu>
2020-01-08 14:42:43 -0500

    one iteration of getting speech mp3

--- src/components/record.js
+++ src/components/record.js
@@ -13,10 +13,13 @@ class Record extends React.Component {
         this.state = {
             record: false,
             sound: '',
+            src: null,
             recorder: null
         }
         this.startRecording = this.startRecording.bind(this)
         this.stopRecording = this.stopRecording.bind(this)
+        this.downloadBlob = this.downloadBlob.bind(this)
+
 
     }
 
@@ -28,6 +31,8 @@ class Record extends React.Component {
 
         this.setState({ recorder: recorder })
     }
+
+
 
     startRecording() {
         // console.log(`This is`, this.state.record)
@@ -41,6 +46,8 @@ class Record extends React.Component {
             console.error(e);
         });
     }
+
+
 
     stopRecording() {
         this.setState({
@@ -57,31 +64,71 @@ class Record extends React.Component {
                     lastModified: Date.now()
                 });
 
-
-
-
                 const player = new Audio(URL.createObjectURL(file));
-
+                //getting src
+                let audioSrc = player.src
+                //passing to state
+                this.setState({ src: audioSrc })
                 player.play();
 
             }).catch((e) => {
                 alert('We could not retrieve your message');
                 console.log(e);
             });
+
+        this.downloadBlob()
     }
 
+
+
+
+
+    downloadBlob(urlObject) {
+
+        // Create a new anchor element
+        const a = document.createElement('a');
+
+        // Set the href and download attributes for the anchor element
+        // You can optionally set other attributes like `title`, etc
+        // Especially, if the anchor element will be attached to the DOM
+        a.href = this.state.src
+        a.download = 'download';
+
+        // Click handler that releases the object URL after the element has been clicked
+        // This is required for one-off downloads of the blob content
+        const clickHandler = () => {
+            setTimeout(() => {
+                a.removeEventListener('click', clickHandler);
+            }, 150);
+        };
+
+        // Add the click event listener on the anchor element
+        // Comment out this line if you don't want a one-off download of the blob content
+        a.addEventListener('click', clickHandler, false);
+
+        // Programmatically trigger a click on the anchor element
+        // Useful if you want the download to happen automatically
+        // Without attaching the anchor element to the DOM
+        // Comment out this line if you don't want an automatic download of the blob content
+        a.click();
+
+        // Return the anchor element
+        // Useful if you want a reference to the element
+        // in order to attach it to the DOM or use it in some other way
+        return a;
+    }
+
+    //not used
     onData(recordedBlob) {
-        const newSound = new Pizzicato.Sound({
-            source: 'wave',
-            options: { path: [recordedBlob.blobURL] }
-        })
 
 
     }
-
+    //not used
     onStop(recordedBlob) {
 
     }
+
+
 
 
 
