950d86d4e755f448b9c89e5e1b9c33cadedf4ffa
Elijah Logan <elijah.logan@purchase.edu>
2020-01-08 01:19:55 -0500

    everything

--- src/components/record.js
+++ src/components/record.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import MicRecorder from 'mic-recorder-to-mp3'
 import { ReactMic } from 'react-mic';
 
 import Pizzicato from 'pizzicato';
@@ -9,11 +10,21 @@ class Record extends React.Component {
         super(props);
         this.state = {
             record: false,
-            sound: ''
+            sound: '',
+            recorder: null
         }
         this.startRecording = this.startRecording.bind(this)
         this.stopRecording = this.stopRecording.bind(this)
         this.playSound = this.playSound.bind(this)
+    }
+
+    componentWillMount() {
+        // New instance
+        const recorder = new MicRecorder({
+            bitRate: 128
+        });
+
+        this.setState({ recorder: recorder })
     }
 
     startRecording() {
@@ -21,14 +32,38 @@ class Record extends React.Component {
         this.setState({
             record: true
         });
-        console.log("Start")
+
+        this.state.recorder.start().then(() => {
+            // something else
+        }).catch((e) => {
+            console.error(e);
+        });
     }
 
     stopRecording() {
         this.setState({
             record: false
         });
-        console.log("Stop")
+        console.log(this.state.record)
+        this.state.recorder
+            .stop()
+            .getMp3().then(([buffer, blob]) => {
+                // do what ever you want with buffer and blob
+                // Example: Create a mp3 file and play
+                const file = new File(buffer, 'me-at-thevoice.mp3', {
+                    type: blob.type,
+                    lastModified: Date.now()
+                });
+                console.log('file downloaded')
+
+
+                const player = new Audio(URL.createObjectURL(file));
+                player.play();
+
+            }).catch((e) => {
+                alert('We could not retrieve your message');
+                console.log(e);
+            });
     }
 
     onData(recordedBlob) {
@@ -36,7 +71,7 @@ class Record extends React.Component {
             source: 'wave',
             options: { path: [recordedBlob.blobURL] }
         })
-        console.log(newSound)
+
 
     }
 
@@ -59,9 +94,8 @@ class Record extends React.Component {
                     onData={this.onData}
                     strokeColor="#000000"
                     backgroundColor="#FF4081" />
-                <button onClick={this.startRecording} type="button">Start</button>
-                <button onClick={this.stopRecording} type="button">Stop</button>
-                <button onClick={this.playSound} type="button">play sound</button>
+                <button onClick={this.startRecording} type="button">record</button>
+                <button onClick={this.stopRecording} type="button">Stop and playback</button>
             </div>
         );
     }
